b23ab94dbc3de236ee8a1dd1747f9b6c72ea112f
zznicole <zznicole@hotmail.com>
2021-05-13 02:48:20 +0200

    update the font color in dark bg

---
+++ dbo.Customers.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Customers] ON
+INSERT INTO [dbo].[Customers] ([Id], [FirstName], [LastName], [Email], [PhoneNo], [BillingAddress], [BillingZipCode], [BillingCity], [BillingCountry], [DeliveryAddress], [DeliveryZipCode], [DeliveryCity], [DeliverCountry]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Customers] OFF
