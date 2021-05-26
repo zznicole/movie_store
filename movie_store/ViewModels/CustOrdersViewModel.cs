666dcfb5579810cf44459e2226c63c4894dfcc67
zznicole <zznicole@hotmail.com>
2021-05-26 20:18:54 +0200

    Create custordersviewmodel

---
+++ movie_store/ViewModels/CustOrdersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using movie_store.Models.DB;
+
+namespace movie_store.ViewModels
+{
+    public class CustOrdersViewModel
+    {
+        public List<OrderViewModel> Orders { get; set; }
+        public Customer Customer { get; set; }
+
+        public CustOrdersViewModel()
+        {
+            Orders = new List<OrderViewModel>();
+        }
+    
+    }
+}
